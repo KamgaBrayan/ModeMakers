ffd1463a0b2d7b708e10cd9a82d2913fdd6ebcd0
leg234-png <wandjilegentia@gmail.com>
2024-12-22 07:28:49 +0100

    ajout de stripe

--- modelmakers/routes/web.php
+++ modelmakers/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripeTestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route pour obtenir le token CSRF
+Route::get('/csrf', function() {
+    return response()->json(['token' => csrf_token()]);
+});
+
+//methode pour tester le paiement avec stripe
+Route::get('/test-stripe', [StripeTestController::class, 'testStripe']);
+Route::post('/create-payment', [StripeTestController::class, 'createPayment']);
+
