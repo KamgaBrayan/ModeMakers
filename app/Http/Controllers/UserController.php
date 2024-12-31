d0c9e618c5841057b84a082c11e7f299c48fbf24
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2024-12-31 12:38:41 +0100

    youser can upload profile picture with route api/user/profile-picture

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Storage;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
@@ -49,4 +50,30 @@ class UserController extends Controller
         return response()->json(['message' => 'User deleted successfully']);
     }
     
+    public function uploadProfilePicture(Request $request)
+    {
+        // Valider le fichier
+        $request->validate([
+            'profile_picture' => 'required|image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+
+        $user = JWTAuth::user();
+
+        // Supprimer l'ancienne photo si elle existe
+        if ($user->profile_picture) {
+            Storage::disk('public')->delete($user->profile_picture);
+        }
+
+        // Sauvegarder la nouvelle photo
+        $path = $request->file('profile_picture')->store('profile_pictures', 'public');
+
+        // Mettre à jour le chemin dans la base de données
+        $user->profile_picture = $path;
+        $user->save();
+
+        return response()->json([
+            'message' => 'Photo de profil mise à jour avec succès.',
+            'profile_picture_url' => Storage::url($path),
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
 
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
@@ -9,4 +11,8 @@ Route::post('auth/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::post('auth/logout', [AuthController::class, 'logout']);
     Route::get('auth/profile', [AuthController::class, 'profile']);
+    Route::post('/user/profile-picture', [UserController::class, 'uploadProfilePicture']);
 });
+
+
+
