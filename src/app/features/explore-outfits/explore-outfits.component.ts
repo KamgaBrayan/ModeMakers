412e2f52d63443fe233b267cf1b6dd4be40eefd6
estelleNdongo <estellendongo02@gmail.com>
2025-01-01 09:42:48 +0100

    page explore-outfits: gestion de tous les components et des responsives

--- src/app/features/explore-outfits/explore-outfits.component.ts
+++ src/app/features/explore-outfits/explore-outfits.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FilterSectionComponent } from 'app/shared/components/filter-section/filter-section.component';
+import { BannerSectionComponent } from 'app/shared/components/banner-section/banner-section.component';
+import { ProductCardComponent } from 'app/shared/components/product-card/product-card.component';
+import { NewsletterSectionComponent } from 'app/shared/components/newsletter-section/newsletter-section.component';
 
 @Component({
   selector: 'app-explore-outfits',
   standalone:true,
   imports: [
     CommonModule,
-    FilterSectionComponent
+    FilterSectionComponent,
+    BannerSectionComponent,
+    ProductCardComponent,
+    NewsletterSectionComponent
   ],
   templateUrl: './explore-outfits.component.html',
   styleUrl: './explore-outfits.component.css'
---
+++ src/app/shared/components/newsletter-section/newsletter-section.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletter-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './newsletter-section.component.html',
+  styleUrl: './newsletter-section.component.css'
+})
+export class NewsletterSectionComponent {
+
+}
