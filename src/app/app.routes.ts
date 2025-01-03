318110c600a4253255d0690d5e5b5854ec71222c
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2025-01-03 06:22:13 +0100

    product list and detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './features/products/products.component';
 import { ProductDetailComponent } from './features/product-detail/product-detail.component';
-import { TestComponent } from './features/test/test.component';
-
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
   },
   { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'test', component: TestComponent}
+  { path: 'products/:id', component: ProductDetailComponent }
 ];
